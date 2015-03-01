b9e7cabc7503d8da2eb765a6aac7e42ca9b96de5
Gilad Oved <giladoved@gmail.com>
2015-02-28 21:00:02 -0500

    added code to get push token

---
+++ Selfless/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Selfless
+//
+//  Created by Gilad Oved on 2/28/15.
+//  Copyright (c) 2015 selfles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Selfless/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Selfless
+//
+//  Created by Gilad Oved on 2/28/15.
+//  Copyright (c) 2015 selfles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
